3b51f7d366a252263a86d4910e40d144a96a3452
Danny Smart Power <dannysmartpower@gmail.com>
2026-09-22 22:04:07 +0100

    Update Firebase configuration and background message logging

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,57 +2,36 @@ importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-
-  apiKey: "AIzaSyB48Z_i8k3gV3uHmNUFQjAH9bOvUlAGb44",
-
+  apiKey: "AIzaSyB48Z_i8k3gV3uHmNUFQjAH9bOvUlAGB44",
   authDomain: "danny-smart-power.firebaseapp.com",
-
+  databaseURL: "https://danny-smart-power-default-rtdb.firebaseio.com",
   projectId: "danny-smart-power",
-
   storageBucket: "danny-smart-power.firebasestorage.app",
-
   messagingSenderId: "649735645158",
-
-  appId: "1:649735645158:web:cff518b89721a163679827"
-
+  appId: "1:649735645158:web:cff518b89721a163679827",
+  measurementId: "G-258YKLP5ZS"
 });
-
 
 const messaging = firebase.messaging();
 
-
 messaging.onBackgroundMessage((payload) => {
-
   console.log(
-    "[firebase-messaging-sw.js] Background message:",
+    "[Danny Smart Power] Background notification received:",
     payload
   );
-
 
   const title =
     payload.notification?.title ||
     "Danny Smart Power";
 
-
   const options = {
-
     body:
       payload.notification?.body ||
       "You have a new notification.",
-
     icon: "/icon-192.png",
-
     badge: "/icon-192.png",
-
-    data:
-      payload.data || {}
-
+    data: payload.data || {}
   };
 
-
-  self.registration.showNotification(
-    title,
-    options
-  );
-
+  self.registration.showNotification(title, options);
 });
